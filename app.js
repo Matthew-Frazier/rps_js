4bad4ff6b51b68390c3766224efa2665e3f62368
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-05-28 14:26:01 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var options = ["rock", "paper", "scissors"];
+
+var wins = []
+var loses = []
+
+var userChoice;
+var compChoice;
+
+var choices = document.getElementsByClassName("choice");
+console.log(choices);
+for(var i = 0; i < choices.length; i++) {
+  choices[i].addEventListener("click", function() {
+    userChoice = this.id;
+    // get the comp choice
+    // display choices
+    // figure out winner
+  });  
+}
