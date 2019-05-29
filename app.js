cf2fae2bec09a061c69fc018655432aaafba52a3
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-05-28 20:43:16 -0600

    styled game and js logic works

--- app.js
+++ app.js
@@ -1,18 +1,104 @@
-var options = ["rock", "paper", "scissors"];
+var options = ["Rock", "Paper", "Scissors"];
 
-var wins = []
-var loses = []
+var result = document.getElementById("result")
+var results_div = document.getElementById("results")
+
+var wins = 0;
+var loses = 0;
+var ties = 0;
+
+var userScore = document.getElementById("user_score");
+var compScore = document.getElementById("comp_score");
+var tieScore = document.getElementById("tie_score");
 
 var userChoice;
 var compChoice;
 
 var choices = document.getElementsByClassName("choice");
-console.log(choices);
+
 for(var i = 0; i < choices.length; i++) {
   choices[i].addEventListener("click", function() {
     userChoice = this.id;
-    // get the comp choice
-    // display choices
-    // figure out winner
-  });  
+    compChoice = options[Math.floor(Math.random() * options.length)]
+    document.getElementById("comp_choice").innerHTML = "The Computer Picked " + compChoice;
+    if (userChoice === "Rock") {
+      if (compChoice === "Paper") {
+        result.innerHTML = "Paper Beats Rock, You Lose!";
+        loses ++;
+        compScore.innerHTML = loses;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('lose_results');  
+      } 
+      else if (compChoice === "Rock") {
+        result.innerHTML = "Rock Vs Rock, There Can Be No Victor!";
+        ties ++;
+        tieScore.innerHTML = ties;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('tie_results'); 
+      } 
+      else {
+        result.innerHTML = "Rock Beats Scissors, You Win!";
+        wins ++;
+        userScore.innerHTML = wins;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('win_results');
+      }
+    }
+    if (userChoice === "Paper") {
+      if (compChoice === "Scissors") {
+        result.innerHTML = "Scissors Beats Paper, You Lose!";
+        loses ++;
+        compScore.innerHTML = loses;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('lose_results'); 
+      } 
+      else if (compChoice === "Paper") {
+        result.innerHTML = "Paper Vs Paper, There Can Be No Victor!";
+        ties ++;
+        tieScore.innerHTML = ties;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('tie_results');
+      } 
+      else {
+        result.innerHTML = "Paper Beats Rock, You Win!";
+        wins ++;
+        userScore.innerHTML = wins;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('win_results');
+      }
+    }
+    if (userChoice === "Scissors") {
+      if (compChoice === "Rock") {
+        result.innerHTML = "Rock Beats Scissors, You Lose!";
+        loses ++;
+        compScore.innerHTML = loses;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('lose_results'); 
+      } 
+      else if (compChoice === "Scissors") {
+        result.innerHTML = "Scissors Vs Scissors, There Can Be No Victor!";
+        ties ++;
+        tieScore.innerHTML = ties;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('tie_results');
+      } 
+      else {
+        result.innerHTML = "Scissors Beats Paper, You Win!";
+        wins ++;
+        userScore.innerHTML = wins;
+        results_div.classList.remove('lose_results', 'win_results', 'tie_results');
+        results_div.classList.add('win_results');
+      }
+    }
+  }); 
 }
+
+// var reset = document.getElementsById("reset");
+// reset.addEventListener("click", function() {
+//   wins = 0;
+//   loses = 0;
+//   ties = 0;
+//   userScore.innerHTML = wins;
+//   compScore.innerHTML = loses;
+//   tieScore.innerHTML = ties;
+// })
